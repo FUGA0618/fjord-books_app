88f65172bd44f59dd0942782bba497889612fce5
user <aax.chiri@gmail.com>
2021-03-21 16:10:44 +0900

    Hashに%記法を使用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:zip, :address, :introduce])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[zip address introduce])
   end
 end
