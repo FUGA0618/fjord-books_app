312180eb88ba91f36c5da0b613b131e86127f19c
user <aax.chiri@gmail.com>
2021-03-24 19:42:48 +0900

    郵便番号、住所、自己紹介文のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :zip, numericality: { only_integer: true }, length: { is: 7 }, allow_nil: true
+  validates :address, length: { maximum: 255 }
+  validates :introduce, length: { maximum: 255 }
 end
