571de59f91b295138eb801c4ee153d238039ec24
user <aax.chiri@gmail.com>
2021-03-29 21:35:13 +0900

    ルーティングをresourcesを使う形に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users
-  get 'users', to: 'users#index'
-  get 'users/:id', to: 'users#show', as: 'user'
+  resources :users, only: %i[index show]
   resources :books
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
