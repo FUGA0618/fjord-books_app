ed404424f9d0286f1a6fdb8e5a81adcec27c407a
user <aax.chiri@gmail.com>
2021-04-06 22:32:38 +0900

    user_iconの国際化に対応

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,6 @@ class User < ApplicationRecord
     return unless user_icon.attached?
 
     allowed_file_types = %w[image/jpg image/jpeg image/gif image/png]
-    errors.add(:user_icon, ', You can upload jpg, jpeg, gif, or png files.') unless allowed_file_types.include?(user_icon.blob.content_type)
+    errors.add(:user_icon, :invalid_file_type, allowed: 'jpg, jpeg, gif, png') unless allowed_file_types.include?(user_icon.blob.content_type)
   end
 end
