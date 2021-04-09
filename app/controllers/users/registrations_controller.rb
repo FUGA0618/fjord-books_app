8c5fb82cc7550e153ffc3a3f2f1aacbc30ebb248
user <aax.chiri@gmail.com>
2021-04-10 07:25:52 +0900

    不要なぼっち演算子を削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,7 +9,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
-    return super if params['password']&.present?
+    return super if params['password'].present?
 
     resource.update_without_password(params.except('current_password'))
   end
