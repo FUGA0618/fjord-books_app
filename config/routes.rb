3f7778679d888d5701852aaf55858c76e69905e3
FUGA0618 <aax.chiri@gmail.com>
2021-04-27 20:13:31 +0900

    Commentのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: "users/registrations" }
   root to: 'books#index'
-  resources :books
-  resources :reports
+  resources :books, :reports do
+    resources :comments, only: %i[edit create update destroy]
+  end
   resources :users, only: %i[index show] do
     resource :relationships, only: %i[create destroy]
     scope module: :users do
