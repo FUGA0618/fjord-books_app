11f7572760a9c3ebd845e06653c396d16b82d586
FUGA0618 <aax.chiri@gmail.com>
2021-05-04 16:38:43 +0900

    users#followed_by?のテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,12 +11,31 @@ class UserTest < ActiveSupport::TestCase
     assert_equal 'Foo Bar', user.name_or_email
   end
 
-  test '#follow' do
+  test '#followed_by?' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    assert_not me.followed_by?(she)
+    she.follow(me)
+    assert me.followed_by?(she)
+  end
+
+  test '#follow_and_#following' do
     me = User.create!(email: 'me@example.com', password: 'password')
     she = User.create!(email: 'she@example.com', password: 'password')
 
     assert_not me.following?(she)
     me.follow(she)
     assert me.following?(she)
+  end
+
+  test '#unfollow' do
+    me = User.create!(email: 'me@example.com', password: 'password')
+    she = User.create!(email: 'she@example.com', password: 'password')
+
+    me.follow(she)
+    assert me.following?(she)
+    me.unfollow(she)
+    assert_not me.following?(she)
   end
 end
