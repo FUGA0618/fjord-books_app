9f7be33bdd5830d208f0c6a31af1b5aaddbb5f0b
FUGA0618 <aax.chiri@gmail.com>
2021-05-04 16:47:06 +0900

    reports#editable?のテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,12 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#editable' do
+    user = User.create!(email: 'foo@example.com', password: 'password')
+    other_user = User.create!(email: 'bar@example.com', password: 'password')
+    report = Report.create!(title: '今日の日報', content: '今日はRubyについて学びました。', user_id: user.id)
+
+    assert_not report.editable?(other_user)
+    assert report.editable?(user)
+  end
 end
