d36c868ddfaef15e5c8a4ffbb86b047c6beb82fc
FUGA0618 <aax.chiri@gmail.com>
2021-05-04 17:01:25 +0900

    reports#created_onのテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -11,4 +11,11 @@ class ReportTest < ActiveSupport::TestCase
     assert_not report.editable?(other_user)
     assert report.editable?(user)
   end
+
+  test '#created_on' do
+    user = User.create!(email: 'foo@example.com', password: 'password')
+    report = Report.create!(title: '今日の日報', content: '今日はRubyについて学びました。', user_id: user.id, created_at: 'Thu, 29 Apr 2021 12:05:26.760615000 JST +09:00')
+
+    assert_equal '2021/04/29', I18n.localize(report.created_on)
+  end
 end
